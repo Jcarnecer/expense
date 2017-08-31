a0c378c7f44677edec4f8f4e4997ed8640dadbc1
Erin Rugas <erinjohnrugas@gmail.com>
2017-08-31 20:17:29 +0800

    mysql

--- expense.sql
+++ expense.sql
@@ -60,7 +60,7 @@ CREATE TABLE `reimbursement` (
   `amount` float(8,2) NOT NULL,
   `receipt_img` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -115,4 +115,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-08-28 23:00:06
+-- Dump completed on 2017-08-31 20:14:58
