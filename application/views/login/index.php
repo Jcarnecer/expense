802e4ecc1ac870bcb2854e03d8ab712b32476871
martslizardo <antoniomartin.lizardo@yahoo.com>
2017-11-20 07:51:10 +0800

    Fixed Login

--- application/views/login/index.php
+++ application/views/login/index.php
@@ -13,7 +13,7 @@
     <script src="assets/js/jquery.min.js"></script>
     <script src="assets/js/bootstrap.min.js"></script>
     <script>
-        var base_url = window.location.origin+"/expense/";
+      
         </script>
     <script src="assets/js/login.js"></script>
 </head>
