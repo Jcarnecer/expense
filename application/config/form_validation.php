3e49161461022ea58031af2bedbf4e8efa3f088a
martslizardo <antoniomartin.lizardo@yahoo.com>
2017-12-12 10:18:42 +0800

    Fixed Add Classification

--- application/config/form_validation.php
+++ application/config/form_validation.php
@@ -46,7 +46,7 @@ $config =
             [
                 'field'		=> 	'classification',
                 'label'   	=> 	'Classification',
-				'rules'  	=> 	'required|regex_match[/^([a-zA-Z]|\s)+$/]|is_unique[classification.classification]',
+				'rules'  	=> 	'required|regex_match[/^([a-zA-Z]|\s)+$/]|is_unique[expense_classification.classification]',
 				'errors'	=> [
 								'regex_match'	=> '%s must be letters only.',
 								'is_unique'		=> '%s is already exist.'
