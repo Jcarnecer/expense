8541eb039792c1a068344292b8273b3fcffb9ccd
martslizardo <antoniomartin.lizardo@yahoo.com>
2018-02-20 15:04:53 +0800

    added icon(sidebar)

--- application/views/include/sidebar.php
+++ application/views/include/sidebar.php
@@ -9,9 +9,20 @@
 		<?php
 		 $position = $this->user->info('role');
 		 $where = array('role_id' => $position);
-		 $permission=$this->Crud_model->fetch('roles_permissions',$where,array('EXP_ADMIN,EXP_EMP')); 
-		
-		if($permission[0]->permission_id=="EXP_ADMIN"){ //human resource (tentative way)?>
+		 $permission=$this->Crud_model->fetch('roles_permissions',$where); 
+		 $privileges=[];
+		 $permission_id=null;
+		 foreach($permission as $row){
+			 $privileges[]=$row->permission_id;
+			 
+		 }
+		 if(in_array('EXP_ADMIN',$privileges)){
+			$permission_id='EXP_ADMIN';
+		 }
+		 elseif(in_array('EXP_EMPLOYEE',$privileges)){
+		   $permission_id='EXP_EMP';
+		 }
+		if($permission_id=="EXP_ADMIN"){ //human resource (tentative way)?>
 			<!-- <li class="sub-menu">
 				<a data-toggle="collapse" href="#expense-sub" aria-expanded="false" aria-controls="expense-sub" >
 					<i class="fa fa-desktop"></i>
@@ -23,18 +34,27 @@
 					
 				</ul>
 			</li> -->
-			<li><a  href="reimbursement">Reimbursement</a></li>
-			<li><a  href="classification">Classification</a></li>
+			<li>
+			  <a  href="reimbursement">
+			     <i class="fa fa-folder-open"></i>
+				 <span>Reimbursements</span>
+			  </a>
+			</li>
+
+			<li>
+				<a  href="classification">
+				<i class="fa fa-table"></i>
+				 <span>Classification</span>
+				</a>
+			</li>
 		<?php } ?>
-			<?php if($permission[0]->permission_id=="EXP_ADMIN" || $permission[0]->permission_id=="EXP_ADMIN"){  //tentative ?>
-				<!-- <li class="sub-menu">
-					<a href="users" >
-						<!-- <i class="fa fa-users"></i> -->
-						<!-- <span>User Management</span>
-					</a>
-				</li> -->
-			<?php } ?> 
-		<li><a  href="request">File Reimbursement</a></li>
+			
+		<li>
+			<a  href="request">
+				<i class="fa fa-edit"></i>
+				 <span>File Reimbursement</span>
+			</a>	
+		</li>
 		
 		<!-- <li><a href="allowance">My Allowances</a></li> -->
 
